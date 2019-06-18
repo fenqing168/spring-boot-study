367adf8c9bcdd66d5a10908e88c162b41e38be5e
wangding <1286976336@qq.com>
2019-06-18 23:07:14 +0800

    45

---
+++ spring-boot-06-data-jdbc/src/main/java/cn/fenqing168/springboot/SpringBoot06DataJdbcApplication.java
@@ -0,0 +1,13 @@
+package cn.fenqing168.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06DataJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataJdbcApplication.class, args);
+    }
+
+}
---
+++ spring-boot-06-data-jdbc/src/main/java/cn/fenqing168/springboot/controller/HelloController.java
@@ -0,0 +1,25 @@
+package cn.fenqing168.springboot.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @ResponseBody
+    @GetMapping("/query")
+    public Map<String, Object> map(){
+        List<Map<String, Object>> list = jdbcTemplate.queryForList("select * from emp");
+        return list.get(0);
+    }
+
+}
