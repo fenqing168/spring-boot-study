c6127fa1f7b66ecef837ce286deff4d85252034a
wangding <1286976336@qq.com>
2019-06-22 19:14:38 +0800

    .

---
+++ spring-boot-06-data-mybatis/src/main/java/cn/fenqing168/springboot/SpringBoot06DataMybatisApplication.java
@@ -0,0 +1,13 @@
+package cn.fenqing168.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot06DataMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot06DataMybatisApplication.class, args);
+    }
+
+}
